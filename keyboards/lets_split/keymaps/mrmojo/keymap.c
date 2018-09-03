09bda1b527fe222fb8539165a1b2d9a59ed7889b
MrMojoR <osvathmarton@gmail.com>
2018-09-03 23:07:55 +0200

    Fixes

--- keyboards/lets_split/keymaps/mrmojo/keymap.c
+++ keyboards/lets_split/keymaps/mrmojo/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-   KC_LCTL, KC_RALT, KC_LGUI, NUMPAD,  LOWER,   KC_INS,      KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+   KC_LCTL, KC_RALT, KC_RGUI, NUMPAD,  LOWER,   KC_INS,      KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Lower
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|     |------+------+------+------+------+------|
  * |      |      |      |      |      |      |     |   4  |   5  |   6  |   *  |      |      |
  * |------+------+------+------+------+------|     |------+------+------+------+------+------|
- * |      |      |      |      |      |      |     |   1  |   2  |   3  |   -  |      |Enter |
+ * |      |      |      |      |      |      |     |   1  |   2  |   3  |   -  |  .   |Enter |
  * |------+------+------+------+------+------|     |------+------+------+------+------+------|
  * |      |      |      |      |      |      |     |  SPC |   0  |  Del |   +  |      |      |
  * `-----------------------------------------'     `-----------------------------------------'
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMPAD] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______,      KC_7,    KC_8,   KC_9,    KC_KP_SLASH,               _______, KC_BSPC, \
   _______, _______, _______, _______, _______, _______,      KC_4,    KC_5,   KC_6,    KC_KP_ASTERISK, _______, _______, \
-  _______, _______, _______, _______, _______, _______,      KC_1,    KC_2,   KC_3,    KC_KP_MINUS,               _______, KC_ENT, \
+  _______, _______, _______, _______, _______, _______,      KC_1,    KC_2,   KC_3,    KC_KP_MINUS,               KC_DOT, KC_ENT, \
   _______, _______, _______, _______, _______, _______,      KC_SPC,  KC_GRV, KC_DEL,  KC_KP_PLUS,                _______, _______  \
 ),    
 
